e5deee2bdf7e91c92ffe2e3a93c4a5a5785d32c3
Ana <zanzmurra@gmail.com>
2025-11-19 23:25:23 +0700

    update tampilan akad resepsi

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
+    // Navbar on scroll
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
             $('.navbar').fadeIn('slow').css('display', 'flex');
@@ -10,16 +10,15 @@
         }
     });
 
-
-    // Smooth scrolling on the navbar links
+    // Smooth scrolling
     $(".navbar-nav a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            
+
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
             }, 1500, 'easeInOutExpo');
-            
+
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
                 $(this).closest('a').addClass('active');
@@ -27,26 +26,23 @@
         }
     });
 
-
     // Modal Video
     $(document).ready(function () {
         var $videoSrc;
         $('.btn-play').click(function () {
             $videoSrc = $(this).data("src");
         });
-        console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
+        $('#videoModal').on('shown.bs.modal', function () {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&modestbranding=1&showinfo=0");
+        });
 
-        $('#videoModal').on('hide.bs.modal', function (e) {
+        $('#videoModal').on('hide.bs.modal', function () {
             $("#video").attr('src', $videoSrc);
-        })
+        });
     });
 
-
-    // Scroll to Bottom
+    // Scroll to Bottom button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.scroll-to-bottom').fadeOut('slow');
@@ -55,20 +51,19 @@
         }
     });
 
-
-    // Portfolio isotope and filter
+    // Portfolio isotope
     var portfolioIsotope = $('.portfolio-container').isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
+
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
@@ -77,41 +72,50 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
-
-    // Gallery carousel
+    // Gallery Carousel
     $(".gallery-carousel").owlCarousel({
         autoplay: false,
         smartSpeed: 1500,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="fa fa-angle-left" aria-hidden="true"></i>',
             '<i class="fa fa-angle-right" aria-hidden="true"></i>'
         ],
         responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            },
-            1200:{
-                items:5
-            }
+            0: { items: 1 },
+            576: { items: 2 },
+            768: { items: 3 },
+            992: { items: 4 },
+            1200: { items: 5 }
         }
     });
-    
-})(jQuery);
 
+
+    function startCountdown() {
+        // Ganti tanggal acara kamu di sini
+        const weddingDate = new Date("2025-12-25T25:00:24").getTime();
+
+        setInterval(() => {
+            const now = new Date().getTime();
+            const distance = weddingDate - now;
+
+            if (distance < 0) return;
+
+            $("#days").text(Math.floor(distance / (1000 * 60 * 60 * 24)));
+            $("#hours").text(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+            $("#minutes").text(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+            $("#seconds").text(Math.floor((distance % (1000 * 60)) / 1000));
+        }, 1000);
+    }
+
+    startCountdown();
+
+})(jQuery);
